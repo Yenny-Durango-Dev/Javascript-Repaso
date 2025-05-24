d027e5d98cd4579e8cfd4a27608f0b6e87a154f9
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2025-05-24 15:54:27 -0500

    tercer commit

--- Basic/02-tiposdedatos.js
+++ Basic/02-tiposdedatos.js
@@ -1,1 +1,25 @@
 // tipos de datos en javascript
+
+// string: cadena de texto, se define con comillas simples, dobles o invertidas
+let nombre = "Yenny"; //Comando para declarar una variable con un string con comillas dobles
+let alias = 'Yelbondiga'; //Comando para declarar una variable con un string con comillas simples
+let email = `correo@gmail.com`; //Comando para declarar una variable con un string con comillas invertidas
+console.log(nombre,alias,email); //Comando para imprimir el valor de la variable en la consola
+
+// number: número, se define con un número
+let age = 20 //Comando para declarar una variable con un número entero
+let height = 1.55 //Comando para declarar una variable con un número decimal
+console.log(age,height); //Comando para imprimir el valor de la variable en la consola
+
+// boolean: booleano, se define con true o false
+let isTrue = true //Comando para declarar una variable con un booleano verdadero
+let isFalse = false //Comando para declarar una variable con un booleano falso
+console.log(isTrue,isFalse); //Comando para imprimir el valor de la variable en la consola
+
+//undefined: indefinido, se define con undefined
+let indefinido //Comando para declarar una variable sin valor
+console.log(indefinido); //Comando para imprimir el valor de la variable en la consola
+
+//null: nulo, se define con null
+let nulo = null //Comando para declarar una variable con un valor nulo
+console.log(nulo); //Comando para imprimir el valor de la variable en la consola

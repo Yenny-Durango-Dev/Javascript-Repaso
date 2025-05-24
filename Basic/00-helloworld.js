36c86fc3c27c1fbbbcd6e946933802f2d4a1c009
Yenny-Durango-Dev <yennycarolinadurango@gmail.com>
2025-05-24 15:45:19 -0500

    segundo commit

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!"); //Comando para imprimir texto en javascript en la consola pero con comillas dobles
+
+console.log('Hola, Mundo!'); //Comando para imprimir texto en la consola pero con comillas simples
+
+console.log(`Hola, Mundo!`); //Comando para imprimir texto en la consola pero con comillas invertidas
+
+// esto es un comentario, en javascript se escribe con (//Texto) y con (/*Texto*/)
+
+console.log(5) //Comando para imprimir un número en la consola
+console.log(5 + 5) //Comando para imprimir una operación matemática en la consola
+console.log(5 - 5) //Comando para imprimir una resta en la consola
+console.log(5 * 5) //Comando para imprimir una multiplicación en la consola
+console.log(5/5) //Comando para imprimir una division en la consola
+console.log("5") 
+console.log("5" + "5") //Comando para imprimir una operación de concatenación en la consola
---
+++ Basic/01-variables.js
@@ -0,0 +1,19 @@
+// variables en javascript
+
+//var
+var helloWorld = "Hello, World!"; //Comando para declarar una variable con var
+console.log(helloWorld); //Comando para imprimir el valor de la variable en la consola
+
+//var: la forma original de javascript para definir variables, es global y puede ser redeclarada y reasignada. Ya no se usa casi porque se puede acceder muy facil a ellas, puede provocar muchos errores hoy en dia y pueden no ser faciles de depurar.
+
+//let
+let helloWorldLet = "Hello, World!"; //Comando para declarar una variable con let
+console.log(helloWorldLet); //Comando para imprimir el valor de la variable en la consola
+
+//let: una forma moderna de definir variables, tiene un alcance de bloque y no puede ser redeclarada en el mismo bloque. La mas usada hoy en dia, ya que es mas segura que var y mas facil de depurar.
+
+//const
+const helloWorldConst = "Hello, World!"; //Comando para declarar una variable con const
+console.log(helloWorldConst); //Comando para imprimir el valor de la variable en la consola
+
+//const: otra forma moderna de definir variables, tiene un alcance de bloque y no puede ser redeclarada ni reasignada. Se usa para definir constantes, es decir, valores que no cambian a lo largo del programa. Es la mas segura de las tres y la mas usada hoy en dia para definir valores que no cambian.
